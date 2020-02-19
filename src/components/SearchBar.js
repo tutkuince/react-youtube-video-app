7175bfa4e78ee568d346cf9f2c3a3aab3a92d09a
tutkuince <tutku.ince@outlook.com>
2020-02-19 15:53:58 +0300

    Handling Form Submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,39 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+
+    state = {
+        term: ''
+    };
+
+
+    onInputChange = (event) => {
+      this.setState({term: event.target.value})
+    };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
     render() {
         return (
-            <div>SearchBar</div>
+            <div className="row">
+                <div className="col col-md-12">
+                    <form onSubmit={this.onFormSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="search">Search</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="search"
+                                value={this.state.term}
+                                onChange={this.onInputChange}
+                            />
+                            <small id="emailHelp" className="form-text text-muted">0</small>
+                        </div>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
